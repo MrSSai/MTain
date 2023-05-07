a76cc146d6b4553747417de6059280156e7f4d32
leo <624647769@qq.com>
2023-05-07 22:27:48 +0800

    新增九宫格插件

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,13 @@
 <template>
-	<view class="content">
+	<view>
+		<uni-search-bar placeholder="请输入需要搜索的内容" @confirm="search" :focus="true" v-model="searchValue">
+		</uni-search-bar>
+<!-- https://1105886.s81i.faiusr.com/2/101/AFEI3r9DEAIYACCyzbbwBSjg0IkIMO4FOKwCQGU.jpg -->
 		<view class="uni-margin-wrap">
 			<swiper class="swiper" circular :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval"
 				:duration="duration">
 				<swiper-item>
+					<!-- https://1105886.s81i.faiusr.com/2/101/AFEI3r9DEAIYACCyzbbwBSjg0IkIMO4FOKwCQGU.jpg -->
 					<view class="swiper-item uni-bg-red">A</view>
 				</swiper-item>
 				<swiper-item>
@@ -14,6 +18,16 @@
 				</swiper-item>
 			</swiper>
 		</view>
+		<uni-section title="维修项目" type="line">
+			<uni-grid :column="2" :highlight="true" @change="change">
+				<uni-grid-item v-for="(item, index) in 8" :index="index" :key="index">
+					<view class="grid-item-box" style="background-color: #fff;">
+						<uni-icons type="image" :size="30" color="#777" />
+						<text class="text">文本信息</text>
+					</view>
+				</uni-grid-item>
+			</uni-grid>
+		</uni-section>
 	</view>
 </template>
 
@@ -25,14 +39,20 @@
 				indicatorDots: true,
 				autoplay: true,
 				interval: 2000,
-				duration: 500
+				duration: 500,
+				searchValue: ''
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
+			search() {
 
+			},
+			input() {
+
+			}
 		}
 	}
 </script>
@@ -60,13 +80,30 @@
 		line-height: 300rpx;
 		text-align: center;
 	}
-	.uni-bg-red{
+
+	.grid-item-box {
+		flex: 1;
+		/* position: relative; */
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 15px 0;
+	}
+
+
+
+	.uni-bg-red {
 		background-color: red;
 	}
-	.uni-bg-green{
+
+	.uni-bg-green {
 		background-color: green;
 	}
-	.uni-bg-blue{
+
+	.uni-bg-blue {
 		background-color: blue;
 	}
 </style>
